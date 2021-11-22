a88cfe50a80655455a3907dc9efe1d89190d917c
maggiben <benjaminmaggi@gmail.com>
2021-11-21 21:47:33 -0300

    chore: input group

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -1,51 +1,32 @@
-import { ReactNode } from 'react';
 import {
   Input,
+  InputGroup,
+  InputRightElement,
   Box,
   Flex,
-  Avatar,
-  Link,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
   useColorModeValue,
   Stack,
   useColorMode,
-  Center,
-} from '@chakra-ui/react';
-import { MoonIcon, SunIcon, SearchIcon } from '@chakra-ui/icons';
-
-const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
-);
+} from '@chakra-ui/react'
+import { MoonIcon, SunIcon, SearchIcon } from '@chakra-ui/icons'
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Logo</Box>
+          <Box>SpaceX Explorer</Box>
 
           <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-              <Input placeholder="Mission name" borderRadius="0"/>
-              <Button colorScheme="blue" borderRadius="0"><SearchIcon /></Button>
+            <Stack direction={'row'} spacing={6}>
+              <InputGroup size="md">
+                <Input pr="12rem" placeholder="Mission name" borderRadius="0" />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm"><SearchIcon /></Button>
+                </InputRightElement>
+              </InputGroup>
               <Button onClick={toggleColorMode}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
               </Button>
@@ -54,5 +35,5 @@ export default function Nav() {
         </Flex>
       </Box>
     </>
-  );
+  )
 }
